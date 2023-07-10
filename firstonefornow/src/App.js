25fd14427736649fd5ec99c2e4ca7e3651cf9896
Sadhguna Aithal <133648612+ape-with-helmet@users.noreply.github.com>
2023-07-10 11:35:45 +0000

    ...

---
+++ firstonefornow/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App(){
+    return(
+        <>
+        <h1 className="bg-primary">hello</h1>
+        <p>jaa acnsancnasc</p>
+        </>
+    )
+}
+
+export default App
